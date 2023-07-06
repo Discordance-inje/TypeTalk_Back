ad34925601a6b0e1d13fab423edb2709e6409f63
dongmin <ehdals45454@naver.com>
2023-07-07 00:34:20 +0900

    feat: JPA, MySQL, and Swagger integration

---
+++ Discordance/src/main/java/com/discordance/discordance/DiscordanceApplication.java
@@ -0,0 +1,13 @@
+package com.discordance.discordance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordanceApplication.class, args);
+    }
+
+}
---
+++ Discordance/src/test/java/com/discordance/discordance/DiscordanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discordance.discordance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
